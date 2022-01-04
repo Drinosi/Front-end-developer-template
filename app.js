efef8d4f3f3f11fac164c68d7cb24aa69f899f4f
Drinosi <91139165+Drinosi@users.noreply.github.com>
2022-01-04 02:31:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -97,7 +97,6 @@ carouselSlide.addEventListener("transitionend", function () {
 window.addEventListener("scroll", reveal);
 
 function reveal() {
-  console.log('hi')
   const reveals = document.querySelectorAll(".reveal");
   for (let i = 0; i < reveals.length; i++) {
     let windowheight = window.innerHeight;
